ec391a4459d92aac4763af84b87daf543c31bcf9
Morgan-Saturn <morgan.tunteva@proton.me>
2026-07-21 16:25:43 +0200

    wip, js added an array with every cell 's value inside

--- scripts.js
+++ scripts.js
@@ -1,4 +1,14 @@
+//affichage des popups
+
 function toggle_popup(popup_id) {
     let popup = document.getElementById(popup_id);
     popup.classList.toggle("hide_popup");
 }
+
+// vérification des valeurs du sudoku
+const node_cellule = document.querySelectorAll('td');
+const cellule = Array.from(node_cellule);
+const sudoku_array = Array.from(node_cellule).map(td => td.innerText);
+
+console.log(sudoku_array);
+
