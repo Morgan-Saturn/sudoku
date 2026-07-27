64f5aba7b15eb7a4bf48d5b3ea85f50e039b410c
Morgan-Saturn <morgan.tunteva@proton.me>
2026-07-27 11:06:21 +0200

    add a function that gets the user the grid's rows depending on the row's index and the grid's size

--- scripts.js
+++ scripts.js
@@ -5,10 +5,30 @@ function toggle_popup(popup_id) {
     popup.classList.toggle("hide_popup");
 }
 
-// vérification des valeurs du sudoku
+//VERIFICATION DES VALEURS DU SUDOKU:
+
+//définition du tableau contenant toutes les valeurs de la grille, de ses colonnes, lignes et boîtes
 const node_cellule = document.querySelectorAll('td');
-const cellule = Array.from(node_cellule);
 const sudoku_array = Array.from(node_cellule).map(td => td.innerText);
 
-console.log(sudoku_array);
+//définition d'une fonction permettant le découpage des lignes pour une grille = un tableau donné prenant en paramètres l'index de la ligne et la longueur de la ligne (9 si grille de 9x9, 4 si grille de 4x4 etc)
+function getRow(board, rowIndex, rowLength) {
+    let tr = board.slice((rowIndex * rowLength), ((rowIndex * rowLength) + rowLength));
+    return tr;
+}
 
+console.log(getRow(sudoku_array, 4, 9));
+
+//récupération des valeurs des colonnes
+/*function getColumn(board, columnIndex) {
+    let column = [];
+    for (let row = 1; row < 10; row ++) {
+        column.push(board[row][columnIndex]);
+    }
+    return column;
+}
+
+
+
+let column_3 = getColumn(sudoku_array, 2);
+console.log(column_3);*/
